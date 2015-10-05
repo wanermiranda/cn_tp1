80258090148e485deefff02b8d8fa44cbd91d70e
Waner Miranda <wanermiranda@gmail.com>
2015-10-05 01:32:40 -0300

    scripts

---
+++ ellipse_noise.sh
@@ -0,0 +1,1 @@
+python2 main.py -f dataset/SR_ellipse_noise.txt -p 100 -g 300 -t 7 -x 0.6 -m 0.3 -e -s
---
+++ yatch.sh
@@ -0,0 +1,1 @@
+python2 main.py -f dataset/yacht_hydrodynamics.txt -p 50 -g 500 -t 7 -x 0.6 -m 0.3 -e -y
